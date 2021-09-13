04b57d9dd71028cbc3949e7af495738916a342cf
Reptil71 <90100015+Reptil71@users.noreply.github.com>
2021-09-13 19:27:53 +0300

    изучали ирадиху

---
+++ RubicTest1/RubicTest1/Cat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubicTest1
+{
+    class Cat
+    {
+        private string Neme = "lexa";
+        private int age = 1;
+        private float height = 30.8f;
+        private bool isBoy = true;
+
+    }
+}
---
+++ RubicTest1/RubicTest1/HOUSE.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubicTest1
+{
+     class HOUSE
+    {
+        private string nameHouse = "хата";
+       private int price = 30303030303030303066600660;
+        private float h = 90.6f;
+       private bool isAmerican =true;
+    }
+}
